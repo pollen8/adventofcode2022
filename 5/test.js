23c4c07019ccd4f8deba9d16c9d4aa4c81410a30
Robert Clayburn <rob@infosum.com>
2022-12-06 01:11:54 +0000

    update tests

--- 5/test.js
+++ 5/test.js
@@ -11,19 +11,19 @@ move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2
  `
-// test('synchronous passing test', (t) => {
-//   // This test passes because it does not throw an exception.
-//   const res = day5.question1(data);
-//   // console.log('res', res);
-//   assert.deepEqual(res, [
-//     ['[C]'],
-//     ['[M]'],
-//     ['[Z]', '[N]', '[D]', '[P]'],
-//   ]);
-// });
+test('question 1', (t) => {
+  // This test passes because it does not throw an exception.
+  const res = day5.question1(data);
+  // console.log('res', res);
+  assert.deepEqual(res, [
+    ['[C]'],
+    ['[M]'],
+    ['[Z]', '[N]', '[D]', '[P]'],
+  ]);
+});
 
 
-test('synchronous passing test', (t) => {
+test('question 2', (t) => {
   // This test passes because it does not throw an exception.
   const res = day5.question2(data);
   // console.log('res', res);
